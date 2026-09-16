5bf5a0e79fc07de7404687bd0d0be1231730872a
jschlos2 <jschlos2@asu.edu>
2026-09-16 14:32:40 -0700

    Add event link preview images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://made-to-gather.pages.dev',
   // Cloudflare Pages serves the generated files directly from dist/.
   output: 'static',
 });
